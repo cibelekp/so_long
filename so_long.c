5a9f5d641dd22a2202cf186ad5b80edd8150e7cb
gitpck <cibele.kp@outlook.com>
2023-08-14 15:39:16 +0100

    reading map

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: ckojima- <ckojima-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 10:58:55 by ckojima-          #+#    #+#             */
-/*   Updated: 2023/08/14 14:03:53 by ckojima-         ###   ########.fr       */
+/*   Updated: 2023/08/14 15:35:42 by ckojima-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,39 @@
 
 int	main(int ac, char **av)
 {
+	int		map_file;
+	char	*temp;
+	char	**map;
+	int		i;
+
 	(void)ac;
 	(void)av;
-	int		map_file;
-	char	*str;
-
 	map_file = open(av[1], O_RDONLY);
 	if (map_file < 0)
 		perror("Error: ");
 	ft_printf("Map fd = %d\n", map_file);
-	str = get_next_line(map_file);
-	ft_printf("%s\n", str);
+	i = 0;
+	map = NULL;
+	temp = NULL;
+	while ((temp = get_next_line(map_file)) != NULL)
+	{
+		ft_printf("%s", temp);
+		map[i] = temp;
+		ft_printf("%s", map[i]);
+		temp = NULL;
+		i++;
+	}
+	// map[i] = NULL;
+	// i = 0;
+	// while (map[i] != NULL)
+	// {
+	// 	ft_printf("%s", map[i]);
+	// 	i++;
+	// }
 	if (map_file != -1)
 	{
 		close(map_file);
-		ft_printf("Closed map fd.\n");
+		ft_printf("\nClosed map fd.\n");
 	}
 	return (0);
 }
