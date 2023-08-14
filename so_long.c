8c9febf513d9da1cdc41e749d1bd8793752ede9b
gitpck <cibele.kp@outlook.com>
2023-08-14 16:58:11 +0100

    check_args

--- so_long.c
+++ so_long.c
@@ -6,15 +6,31 @@
 /*   By: ckojima- <ckojima-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 10:58:55 by ckojima-          #+#    #+#             */
-/*   Updated: 2023/08/14 15:35:42 by ckojima-         ###   ########.fr       */
+/*   Updated: 2023/08/14 16:45:06 by ckojima-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-// void	check_map(int fd)
-// {
-// }
+int	check_args(int ac, char *av)
+{
+	size_t	i;
+
+	if (ac != 2)
+	{
+		ft_printf("Error: wrong arguments. Usage: ./so_long <map.ber>\n");
+		return (-1);
+	}
+	i = 0;
+	i = (ft_strlen(av)) - 4;
+	// ft_printf("&av[1][%d] = %s\n", i, &av[i]);
+	if ((ft_strncmp(&av[i], ".ber", 4)) != 0)
+	{
+		ft_printf("Error: map must have the extention .ber\n");
+		return (-1);
+	}
+	return (0);
+}
 
 int	main(int ac, char **av)
 {
@@ -23,22 +39,26 @@ int	main(int ac, char **av)
 	char	**map;
 	int		i;
 
-	(void)ac;
-	(void)av;
+	if (check_args(ac, av[1]) != 0)
+	{
+		ft_printf("Exiting program.\n");
+		return (-1);
+	}
 	map_file = open(av[1], O_RDONLY);
 	if (map_file < 0)
 		perror("Error: ");
-	ft_printf("Map fd = %d\n", map_file);
 	i = 0;
 	map = NULL;
 	temp = NULL;
+	// CHECK MAP RULES BEFORE ALLOCATING MEMORY?
+	// CREATE MATRIX
 	while ((temp = get_next_line(map_file)) != NULL)
 	{
 		ft_printf("%s", temp);
-		map[i] = temp;
-		ft_printf("%s", map[i]);
+		// map[i] = temp;
+		// ft_printf("%s", map[i]);
 		temp = NULL;
-		i++;
+		// i++;
 	}
 	// map[i] = NULL;
 	// i = 0;
@@ -47,6 +67,8 @@ int	main(int ac, char **av)
 	// 	ft_printf("%s", map[i]);
 	// 	i++;
 	// }
+	
+	// LATER: CLEANUP FUNCTION
 	if (map_file != -1)
 	{
 		close(map_file);
