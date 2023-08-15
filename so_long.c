da977cb525bf72a3938c1003f360001c05a23b63
gitpck <cibele.kp@outlook.com>
2023-08-15 18:44:58 +0100

    map matrix

--- so_long.c
+++ so_long.c
@@ -6,11 +6,18 @@
 /*   By: ckojima- <ckojima-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 10:58:55 by ckojima-          #+#    #+#             */
-/*   Updated: 2023/08/14 16:45:06 by ckojima-         ###   ########.fr       */
+/*   Updated: 2023/08/15 18:43:25 by ckojima-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+t_map	*map(void)
+{
+	static t_map	map_p;
+
+	return (&map_p);
+}
 
 int	check_args(int ac, char *av)
 {
@@ -32,47 +39,53 @@ int	check_args(int ac, char *av)
 	return (0);
 }
 
+void	map_matrix(int map_fd)
+{
+	char	*map_row;
+	int		rows;
+
+	map_row = NULL;
+	rows = 0;
+	map()->matrix = (char **)malloc(sizeof(char *) * (rows + 1));
+	while ((map_row = get_next_line(map_fd)) != NULL)
+	{
+		map()->matrix[rows] = map_row;
+		ft_printf("map.matrix[%d] %s", rows, map()->matrix[rows]);
+		rows++;
+	}
+	map()->matrix[rows] = NULL;
+	ft_printf("\nmap.matrix[%d] %s", rows, map()->matrix[rows]);
+	map()->height = rows;
+	ft_printf("\nmap.height = %d\n", map()->height);
+}
+
 int	main(int ac, char **av)
 {
-	int		map_file;
-	char	*temp;
-	char	**map;
-	int		i;
+	int	map_fd;
+	int	x;
 
 	if (check_args(ac, av[1]) != 0)
 	{
-		ft_printf("Exiting program.\n");
+		ft_printf("Call exit_program.\n");
 		return (-1);
 	}
-	map_file = open(av[1], O_RDONLY);
-	if (map_file < 0)
+	map_fd = open(av[1], O_RDONLY);
+	if (map_fd < 0)
 		perror("Error: ");
-	i = 0;
-	map = NULL;
-	temp = NULL;
-	// CHECK MAP RULES BEFORE ALLOCATING MEMORY?
+	map()->height = 0;
 	// CREATE MATRIX
-	while ((temp = get_next_line(map_file)) != NULL)
+	map_matrix(map_fd);
+	// LATER: CLEANUP FUNCTION	ft_printf("\nmap.height = %d", map()->height);
+	x = 0;
+	while (map()->height >= 0)
 	{
-		ft_printf("%s", temp);
-		// map[i] = temp;
-		// ft_printf("%s", map[i]);
-		temp = NULL;
-		// i++;
+		ft_printf("map.matrix[%d] %s\n", x, map()->matrix[x]);
+		x++;
+		map()->height -= 1;
 	}
-	// map[i] = NULL;
-	// i = 0;
-	// while (map[i] != NULL)
-	// {
-	// 	ft_printf("%s", map[i]);
-	// 	i++;
-	// }
-	
-	// LATER: CLEANUP FUNCTION
-	if (map_file != -1)
 	{
-		close(map_file);
-		ft_printf("\nClosed map fd.\n");
+		close(map_fd);
+		// ft_printf("\nClosed map fd.\n");
 	}
 	return (0);
 }
--- so_long.h
+++ so_long.h
@@ -1,12 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckojima- <ckojima-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 11:39:05 by ckojima-          #+#    #+#             */
+/*   Updated: 2023/08/15 18:06:44 by ckojima-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "./libft/libft.h"
 # include "./ft_printf/ft_printf.h"
 # include "./gnl/get_next_line.h"
+# include "./libft/libft.h"
 # include <fcntl.h>
 # include <stdio.h>
 # include <unistd.h>
 
+typedef struct		map
+{
+	int		fd;
+	char	**matrix;
+	int		height;
+} t_map;
 
 #endif
